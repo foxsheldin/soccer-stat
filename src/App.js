95c5e117098aab372710467d1337d356ff659a4c
foxsheldin <fruktsky@gmail.com>
2022-02-07 11:11:57 +0300

    Измененение Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-/* import './App.css'; */
 import PageLayout from './components/common/PageLayout/PageLayout';
 import CompetitionsContainer from './components/Competitions/CompetitionsContainer';
-import TeamsContainer from './components/Teams/TeamsContainer';
 import TeamCalendarContainer from './components/TeamCalendar/TeamCalendarContainer';
 import CompetitionCalendarContainer from './components/CompetitionCalendar/CompetitionCalendarContainer';
 
@@ -13,9 +11,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<PageLayout />} >
           <Route path='/competitions' element={<CompetitionsContainer />} />
-          <Route path='/teams' element={<TeamsContainer />} />
           <Route path='/teams/:teamid' element={<TeamCalendarContainer />} />
-          <Route path='/competition-calendar' element={<CompetitionCalendarContainer />} />
+          <Route path='/competitions/:competitionCode/teams' element={<CompetitionCalendarContainer />} />
         </Route>
       </Routes>
     </div>
