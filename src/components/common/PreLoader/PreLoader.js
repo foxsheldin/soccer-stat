8af8e83784a0d4043f5597fb0d1129b8d1b6ce86
foxsheldin <fruktsky@gmail.com>
2022-02-07 11:13:39 +0300

    Добавлен PreLoader

---
+++ src/components/common/PreLoader/PreLoader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import preloader from '../../../assets/images/Eclipse-1s-200px.svg';
+
+function PreLoader() {
+    return (
+        <div style={{textAlign: 'center'}}>
+            <img src={preloader} />
+        </div>
+    )
+}
+
+export default PreLoader
