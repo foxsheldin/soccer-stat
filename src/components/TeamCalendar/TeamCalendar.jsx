77f0ff4eecf1e43e31467a63092c033edfda3d35
foxsheldin <fruktsky@gmail.com>
2022-02-07 11:17:26 +0300

    Изменен компонент TeamCalendar

--- src/components/TeamCalendar/TeamCalendar.jsx
+++ src/components/TeamCalendar/TeamCalendar.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import PreLoader from "../common/PreLoader/PreLoader";
+import MathcesItem from "../common/MatchesItem/MatchesItem";
 
-const TeamCalendar = () => {
+const TeamCalendar = (props) => {
+  const matchesElements = (matches) => {
+    return matches.map((each) => <MathcesItem {...each} />)
+  }
+
   return <main className='main-team-calendar'>
   <div className='main-team-calendar__wrapper'>
-    Календарь команды
+    <div className='about-team'>
+      <div className='team-name team-logo'>
+        {props.team.creastUrl ? <img src={props.team.crestUrl} alt={'Логотип '+props.team.name} className='logo'/> : null}
+        <span className='name'>{props.team.name}</span>
+      </div>
+      <div className='team-contacts'>
+        {props.team.website ? <div className='contacts-website'><a href={props.team.website}>Официальный сайт команды</a></div> : null}
+        {props.team.email ? <div className='contacts-email'>Email: {props.team.email}</div> : null}
+        {props.team.phone ? <div className='contacts-phone'>Телефон: {props.team.phone}</div> : null}
+        {props.team.address ? <div className='contacts-address'>Адрес: {props.team.address}</div> : null}
+      </div>
+    </div>
+    <div className='calendar-team'>
+      <p>Календарь команды</p>
+      {props.countMatches !== null ?
+          props.countMatches ? <table>
+                <thead>
+                <tr>
+                  <th>Дата игры</th>
+                  <th>Команды</th>
+                  <th>Счет</th>
+                  <th>Пенальти</th>
+                  <th>Cтатус игры</th>
+                </tr>
+                </thead>
+                <tbody>
+                {matchesElements(props.matches)}
+                </tbody>
+              </table>
+
+              : 'Нет существующих игр'
+
+          : <PreLoader />}
+    </div>
   </div>
-</main>;;
+</main>;
 };
 
 export default TeamCalendar;
--- src/components/TeamCalendar/TeamCalendarContainer.jsx
+++ src/components/TeamCalendar/TeamCalendarContainer.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import './style.css';
 import TeamCalendar from './TeamCalendar';
+import {connect} from "react-redux";
+import {getOneTeam, getTeamMatches} from "../../redux/teams-reducer";
+import {useParams} from "react-router-dom";
 
-const TeamCalendarContainer = () => {
-  return <TeamCalendar />;
+const TeamCalendarContainer = (props) => {
+  const {teamid} = useParams();
+
+  useEffect(() => {
+    props.getOneTeam(teamid);
+    props.getTeamMatches(teamid);
+  }, [])
+
+  return <TeamCalendar team={props.team} matches={props.matches}
+                       countMatches={props.countMatches}/>;
 };
 
-export default TeamCalendarContainer;
+const mapStateToProps = (state) => {
+  return {
+    team: state.teamsState.team,
+    matches: state.teamsState.matches,
+    countMatches: state.teamsState.countMatches,
+  }
+}
+
+export default connect(mapStateToProps,
+    {getOneTeam, getTeamMatches})(TeamCalendarContainer);
