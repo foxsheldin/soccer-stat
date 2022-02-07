1013672a865a03bc589a7f81886eb7baafdd9ff9
foxsheldin <fruktsky@gmail.com>
2022-02-07 11:17:00 +0300

    Изменен компонент CompetitionCalendar

--- src/components/CompetitionCalendar/CompetitionCalendar.jsx
+++ src/components/CompetitionCalendar/CompetitionCalendar.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import './style.css';
+import PreLoader from "../common/PreLoader/PreLoader";
+import {Link} from "react-router-dom";
+import MathcesItem from "../common/MatchesItem/MatchesItem";
 
-const CompetitionCalendar = () => {
-  return <main className='main-competition-calendar'>
-  <div className='main-competition-calendar__wrapper'>
-    Календарь лиги
-  </div>
-</main>;
+const TeamsItem = (props) => {
+    return <Link to={'/teams/'+props.id} className='team-list-item'>{props.name}</Link>
+}
+
+const CompetitionCalendar = (props) => {
+    const matchesElements = (matches) => {
+        return matches.map((each) => <MathcesItem {...each} />)
+    }
+
+    const teamsElements = (teams) => {
+        return teams.map((each) => <TeamsItem {...each} />)
+    }
+
+    return <main className='main-competition-calendar'>
+        <div className='main-competition-calendar__wrapper'>
+            <div className='teams-list'>
+                <h4>Список команд лиги</h4>
+                {props.countTeams !== null ?
+                    props.countTeams ?
+                        <>{teamsElements(props.teams)}</>
+
+                        : 'Список команд не определен'
+
+                    : <PreLoader />}
+            </div>
+            <div className='calendar-list-table'>
+                <h4>Календарь лиги</h4>
+                {props.countMatches !== null ?
+                    props.countMatches ? <table>
+                        <thead>
+                        <tr>
+                            <th>Дата игры</th>
+                            <th>Команды</th>
+                            <th>Счет</th>
+                            <th>Пенальти</th>
+                            <th>Cтатус игры</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {matchesElements(props.matches)}
+                        </tbody>
+                    </table>
+
+                    : 'Нет существующих игр'
+
+                : <PreLoader />}
+            </div>
+        </div>
+    </main>;
 };
 
 export default CompetitionCalendar;
--- src/components/CompetitionCalendar/CompetitionCalendarContainer.jsx
+++ src/components/CompetitionCalendar/CompetitionCalendarContainer.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { compose } from 'redux';
+import { getMatches } from '../../redux/competitions-reducer';
 import CompetitionCalendar from './CompetitionCalendar';
+import {getTeams} from "../../redux/teams-reducer";
 
-const CompetitionCalendarContainer = () => {
-  return <CompetitionCalendar />;
+
+const CompetitionCalendarContainer = (props) => {
+  const {competitionCode} = useParams();
+  useEffect(() => {
+    props.getMatches(competitionCode);
+    props.getTeams(competitionCode);
+  },[]);
+
+  return <CompetitionCalendar matches={props.matches} countMatches={props.countMatches}
+                              teams={props.teams} countTeams={props.countTeams}/>;
 };
 
-export default CompetitionCalendarContainer;
+const mapStateToProps = (state) => {
+  return {
+    matches: state.competitionsState.matches,
+    countMatches: state.competitionsState.countMatches,
+    teams: state.teamsState.teams,
+    countTeams: state.teamsState.countTeams,
+  }
+}
+
+export default compose(
+  connect(mapStateToProps, { getMatches, getTeams })
+)(CompetitionCalendarContainer);
