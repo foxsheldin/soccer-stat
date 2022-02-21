1f85d069fb1d31cbb9b2f162e2bc012c1efd8610
foxsheldin <fruktsky@gmail.com>
2022-02-22 00:07:57 +0300

    Стилизация NavBar согласно ТЗ

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './style.css';
+import './style.scss';
 
 const NavBar = (props) => {
-  const setActive = ({isActive}) => isActive ?'navbar-list__item_active navbar-list__item':'navbar-list__item'
-
   return <header className='header'>
     <div className='header__wrapper'>
       <NavLink to='/' className='header__logo'>
         SoccerStat
       </NavLink>
       <nav className='header__navbar navbar-list'>
-        <NavLink to="/competitions" className={setActive}><li >Лиги</li></NavLink>
-        <NavLink to="/competitions/PL/teams" className={setActive}><li>Календарь лиги</li></NavLink>
-        <NavLink to="/competitions/PL/teams" className={setActive}><li>Команды</li></NavLink>
-        <NavLink to="/teams/57" className={setActive}><li>Команда</li></NavLink>
+        <NavLink to="/competitions" className='navbar-list__item'><li >Лиги</li></NavLink>
+        <NavLink to="/teams" className='navbar-list__item'><li>Команды</li></NavLink>
       </nav>
     </div>
   </header>;
