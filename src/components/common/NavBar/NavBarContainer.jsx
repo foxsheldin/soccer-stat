e100b36e724f3e4d610884517292e7e0a996278f
foxsheldin <fruktsky@gmail.com>
2022-02-22 00:01:18 +0300

    Исправление ошибок NavBarContainer

--- src/components/common/NavBar/NavBarContainer.jsx
+++ src/components/common/NavBar/NavBarContainer.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
 
 const NavBarContainer = React.memo(() => {
     const location = useLocation();
     const navigate = useNavigate();
+
+    useEffect(()=>{
+        if (location.pathname === '/') {
+            navigate("/competitions");
+        }
+    }, [location.pathname]);
     
-    if (location.pathname === '/') {
-        navigate("/competitions");
-    }
     return <NavBar />;
 });
 
