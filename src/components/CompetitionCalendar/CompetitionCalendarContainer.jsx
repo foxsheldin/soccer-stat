7c3c2481d6f8701715dfc2c9496fd19b379ebc68
foxsheldin <fruktsky@gmail.com>
2022-02-22 00:21:25 +0300

    Удалены лишние запросы в CompetitionCalendarCont

--- src/components/CompetitionCalendar/CompetitionCalendarContainer.jsx
+++ src/components/CompetitionCalendar/CompetitionCalendarContainer.jsx
@@ -4,29 +4,24 @@ import { useParams } from 'react-router-dom';
 import { compose } from 'redux';
 import { getMatches } from '../../redux/competitions-reducer';
 import CompetitionCalendar from './CompetitionCalendar';
-import {getTeams} from "../../redux/teams-reducer";
 
 
 const CompetitionCalendarContainer = (props) => {
   const {competitionCode} = useParams();
   useEffect(() => {
     props.getMatches(competitionCode);
-    props.getTeams(competitionCode);
   },[]);
 
-  return <CompetitionCalendar matches={props.matches} countMatches={props.countMatches}
-                              teams={props.teams} countTeams={props.countTeams}/>;
+  return <CompetitionCalendar matches={props.matches} countMatches={props.countMatches} />;
 };
 
 const mapStateToProps = (state) => {
   return {
     matches: state.competitionsState.matches,
     countMatches: state.competitionsState.countMatches,
-    teams: state.teamsState.teams,
-    countTeams: state.teamsState.countTeams,
   }
 }
 
 export default compose(
-  connect(mapStateToProps, { getMatches, getTeams })
+  connect(mapStateToProps, { getMatches })
 )(CompetitionCalendarContainer);
