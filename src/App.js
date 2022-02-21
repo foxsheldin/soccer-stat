2f1b631702d46e0983549f44431917596832b77c
foxsheldin <fruktsky@gmail.com>
2022-02-22 00:14:35 +0300

    Изменены Route согласно ТЗ

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PageLayout from './components/common/PageLayout/PageLayout';
 import CompetitionsContainer from './components/Competitions/CompetitionsContainer';
 import TeamCalendarContainer from './components/TeamCalendar/TeamCalendarContainer';
 import CompetitionCalendarContainer from './components/CompetitionCalendar/CompetitionCalendarContainer';
+import TeamsContainer from './components/Teams/TeamsContainer';
 
 const App = () => {
   return (
@@ -11,8 +12,9 @@ const App = () => {
       <Routes>
         <Route path='/' element={<PageLayout />} >
           <Route path='/competitions' element={<CompetitionsContainer />} />
+          <Route path='/teams' element={<TeamsContainer />} />
           <Route path='/teams/:teamid' element={<TeamCalendarContainer />} />
-          <Route path='/competitions/:competitionCode/teams' element={<CompetitionCalendarContainer />} />
+          <Route path='/competitions/:competitionCode' element={<CompetitionCalendarContainer />} />
         </Route>
       </Routes>
     </div>
