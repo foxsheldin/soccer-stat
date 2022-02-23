78c697f2b21b1bdc39fdaa5f5e7ec9734fbf0e98
foxsheldin <fruktsky@gmail.com>
2022-02-23 17:16:01 +0300

    Добавлена страница Команды

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import './style.css';
+import { Link } from 'react-router-dom';
+import PreLoader from '../common/PreLoader/PreLoader';
+import './style.scss';
 
-const Teams = () => {
+const TeamsItem = (props) => {
+  const addCodeLink = props.code??props.id;
+
+  return <Link to={`/team/${addCodeLink}`}>
+    <div className='teams-list__item'>
+      <div className='teams__name'>{props.name}</div>
+      <div className='teams__logo'>
+        {props.area.crestUrl || props.crestUrl ?
+          <img src={props.area.crestUrl ?? props.crestUrl} width='150' height='150' />
+        : null}
+      </div>
+    </div>
+  </Link>
+}
+
+const teamsElements = (teams) => {
+  return teams.map((each) => <TeamsItem {...each} />)
+}
+
+const Teams = (props) => {
   return <main className='main-teams'>
-  <div className='main-teams__wrapper'>
-    Команды
+  <div className='main-wrapper'>
+    <h1 className='visually-hidden'>Лиги</h1>
+    <div className='search'>
+      <input type='search' placeholder='Поиск команды' onChange={props.onSearchChange}/>
+    </div>
+    {props.count !== null ? 
+      <div className='teams-list'>
+        {teamsElements(props.teams)}
+      </div>
+    : <PreLoader />}
   </div>
 </main>;
 };
--- src/components/Teams/TeamsContainer.jsx
+++ src/components/Teams/TeamsContainer.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import Teams from './Teams';
+import { getTeams } from '../../redux/teams-reducer';
+import { useParams } from 'react-router-dom';
 
-const TeamsContainer = () => {
-  return <Teams />;
+const TeamsContainer = (props) => {
+  const {page} = useParams();
+
+  useEffect(() => {
+    props.getTeams();
+  }, []);
+  
+  const onSearchChange = () => {
+    console.log('searchChanged');
+  }
+
+  return <Teams teams={props.teams} count={props.countTeams} onSearchChange={onSearchChange}/>;
 };
 
-export default TeamsContainer;
+const mapStateToProps = (state) => {
+  return {
+    teams: state.teamsState.teams,
+    countTeams: state.teamsState.countTeams,
+  }
+}
+
+export default connect(mapStateToProps, { getTeams })(TeamsContainer);
